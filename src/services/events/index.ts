bc1e83d0cf9b450a416c9012c85fae5cc3ee0f70
ROG <luan.roger.2003@gmail.com>
2026-05-24 14:29:47 -0300

    feat: start create live games page

---
+++ src/services/events/index.ts
@@ -0,0 +1,31 @@
+import { EVENTS_API_URL } from "astro:env/server";
+import type { SportByIdResponse, SportsResponse } from "./interfaces";
+
+export async function getLiveEvents(): Promise<SportsResponse> {
+  const baseUrl = EVENTS_API_URL;
+  console.log(baseUrl);
+  const response = await fetch(
+    `${baseUrl}/sports?category=Futebol&status=live`,
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+export async function getUpcomingEvents(): Promise<SportsResponse> {
+  const baseUrl = EVENTS_API_URL;
+  const response = await fetch(
+    `${baseUrl}/sports?category=Futebol&status=upcoming`,
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+export async function getEventById(id: string): Promise<SportByIdResponse> {
+  const baseUrl = EVENTS_API_URL;
+  const response = await fetch(`${baseUrl}/sports/${id}`);
+
+  const data = await response.json();
+  return data;
+}
